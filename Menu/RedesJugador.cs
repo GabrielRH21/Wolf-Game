8c5c81b1c9a6998bd1d8a2c428ac82d15e5b663f
GabrielRH21 <alu0101216829@ull.edu.es>
2023-07-23 21:47:38 +0100

    Aumento del conteo de jugadores en el lobby

--- Menu/RedesJugador.cs
+++ Menu/RedesJugador.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 using Photon.Pun;
 
 public class RedesJugador : MonoBehaviour
@@ -15,10 +17,26 @@ public class RedesJugador : MonoBehaviour
         codigo.enabled = false;
       }
     }
+    updatePlayers();
   }
 
   void Update()
   {
 
+  }
+
+  void updatePlayers() {
+    GameObject[] players  = GameObject.FindGameObjectsWithTag("Texteable");
+    if  (players[0] != null) {
+      string playersText = players[0].GetComponent<Text>().text;
+      string num;
+      num =  playersText[playersText.Length-1].ToString();
+      if (int.TryParse(playersText[playersText.Length-2].ToString(), out _)) {
+        num = playersText[playersText.Length-2].ToString() +  playersText[playersText.Length-1].ToString();
+      }
+      int result = int.Parse(num);
+      result++;
+      players[0].GetComponent<Text>().text = "Jugadores: " + result;
+    }
   }
 }
